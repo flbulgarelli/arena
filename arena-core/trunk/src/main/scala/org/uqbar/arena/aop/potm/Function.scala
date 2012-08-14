6cca3b6db0873b50f826d0853b4221d39b3fadba
tesonep <tesonep@e1a88654-4610-4beb-b2e6-8ed2f74c6698>
2012-08-14 18:51:44 +0000

    git-svn-id: https://xp-dev.com/svn/uqbar/projects/arena@311 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- arena-core/trunk/src/main/scala/org/uqbar/arena/aop/potm/Function.scala
+++ arena-core/trunk/src/main/scala/org/uqbar/arena/aop/potm/Function.scala
@@ -4,7 +4,7 @@ import org.uqbar.lacar.ui.model.Action;
 import org.uqbar.commons.utils.ReflectionUtils
 
 class Function[A](var method: (A*) => Unit) extends Action {
-  override def execute() = method(Seq())
-  override def execute[B](objects: B*) = objects.head match { case p: A => method(Seq(p)) }
-  def execute(a: A) = method(Seq(a))
+  override def execute() = method()
+  override def execute[B](objects: B*) = objects.head match { case p: A => method(p) }
+  def execute(a: A) = method(a)
 }
